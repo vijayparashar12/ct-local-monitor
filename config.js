b1fdad3f1bbee02d9f76482b436bc7d3bc31ad89
vijay <vijayparashar.eca@gmail.com>
2016-05-29 01:55:10 +0530

    added lat lng to cities

--- config.js
+++ config.js
@@ -1,3 +1,54 @@
 module.exports = {
-    cities: ["bangalore", "mumbai", "new delhi", "chennai", "hyderabad","jaipur","kolkata"]
+    cities: [
+        {
+            name: "bangalore",
+            location: { lat: 12.9716, lng: 77.5946 }
+        },
+        {
+            name: "mumbai",
+            location: { lat: 19.076, lng: 72.8777 }
+        },
+        {
+            name: "new delhi",
+            location: {lat: 28.6139, lng: 77.2090}
+        },
+        {
+            name: "chennai",
+            location: { lat: 13.0827, lng: 80.2707 }
+        },
+        {
+            name: "hyderabad",
+            location: { lat: 17.3850, lng: 78.4867 }
+        },
+        {
+            name: "jaipur",
+            location: { lat: 26.9124, lng: 75.7873 }
+        },
+        {
+            name: "kolkata",
+            location: { lat: 22.5726, lng: 88.3639 }
+        },
+        {
+            name: "ahmedabad",
+            location: { lat: 23.0225, lng: 72.5714 }
+        },
+        {
+            name: "goa",
+            location: { lat: 15.2993, lng: 74.1240 }
+        },
+        {
+            name: "pune",
+            location: { lat: 18.5204, lng: 73.8567 }
+        },
+        {
+            name: "dubai",
+            location: { lat: 25.2048, lng: 55.2708 }
+        }
+    ],
+    products: [
+        "ttd",
+        "fnb",
+        "events",
+        "fitness"
+    ]
 }
